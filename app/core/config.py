c74948182bdcf2d6c7ea5853d89f8169c3beed3f
Olga Chirkova <lely4e@gmail.com>
2025-10-21 13:20:57 +0200

    Config added

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from dotenv import find_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=find_dotenv(),
+        env_file_encoding="utf-8",
+    )
+
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def SYNC_DATABASE_URL(self):
+        return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+settings = Settings()

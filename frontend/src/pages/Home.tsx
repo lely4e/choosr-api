fe5dd7c255e085ea088ad9d13248b14c29839071
Olga Chirkova <lely4e@gmail.com>
2026-01-19 11:21:34 +0100

    Home page added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,21 @@
-// import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
 
     return (
         <>
-            <h1>Welcome to main page!</h1>
+
+    <div className="wrap-home">
+            <h1 className="main-text">Make Group Decisions<div style={{color: '#FF6A00'}}>Effortlessly</div></h1>
+            <div className="main-text-description">Create polls, share product links, and let your team vote on the best option. Perfect for event planning, team gifts, and group purchases.</div>
             {/* <div className="login-signup-buttons">
             <button onClick={() => navigate("/login")} type="button">Login</button>
             <button onClick={() => navigate("/signup")} type="button">Signup</button>
             </div> */}
+            <button onClick={() => navigate("/login")} className="main-page-button">Create New Event</button>
+          </div>
+
         </>
     )
 }

5fdf29ed62c2fae9e74bbfe4005ba1cebbf03110
Olga Chirkova <lely4e@gmail.com>
2026-01-19 11:19:56 +0100

    Navbar updated

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,18 +6,18 @@ export default function Navbar() {
             <div className="menu">
                 <div className="logo">
                 <Link to="/">
-                <img src="../src/assets/logo.svg" alt="logo" width={120}/>
+                <img src="../src/assets/logo_orange.svg" alt="logo" width={120}/>
                 </Link>
                 </div>
                 {/* <a href="/">HOME</a> */}
                 <div className="menu-text">
-                <a href="/my-polls">POLLS</a>
-                <a href="">SEARCH</a>
-                <a href="">GIFT IDEAS</a>
-                <a href="">SAVED</a>
+                <a href="/my-polls">Events</a>
+                <a href="/search">Search</a>
+                <a href="/ideas">Gift Ideas</a>
+                <a href="/saved">Saved</a>
                 {/* <a href="">PROFILE</a> */}
-                <a href="/login">LOG IN</a>
-                <a href="/signup">SIGN UP</a>
+                <a href="/login">Sign in</a>
+                {/* <a href="/signup">Sign up</a> */}
                 <Link to="/profile">
                 <img src="../src/assets/profile.svg" alt="profile" width={40}/>
                 </Link>

623bba494287ddd9483497079f9dd4fae5e70492
Olga Chirkova <lely4e@gmail.com>
2026-01-21 17:37:09 +0100

    Pydantic model updated

--- app/api/routes/search.py
+++ app/api/routes/search.py
@@ -1,5 +1,6 @@
-from app.services.products import get_items_test, get_items_from_API
-from fastapi import APIRouter, Depends, Request, Query
+from app.api.schemas.product import ProductSearchOut
+from app.services.products import get_items_test
+from fastapi import APIRouter, Depends, Query
 from app.core.security import oauth2_scheme
 from fastapi.concurrency import run_in_threadpool
 
@@ -7,10 +8,21 @@ from fastapi.concurrency import run_in_threadpool
 search_router = APIRouter(dependencies=[Depends(oauth2_scheme)])
 
 
-@search_router.get("/products/search")
-async def read_products_query(search: str = Query(..., min_length=2)):
+# @search_router.get("/products/search", response_model=list[ProductSearchOut])
+# async def read_products_query(
+#     search: str = Query(..., min_length=2)
+# ) -> ProductSearchOut:
+#     """Searching product using Amazon Search API"""
+#     products = await run_in_threadpool(get_items_from_API, search)
+#     return products
+
+
+@search_router.get("/products/search", response_model=list[ProductSearchOut])
+async def read_products_query(
+    search: str = Query(..., min_length=2)
+) -> ProductSearchOut:
     """Searching product using Amazon Search API"""
-    products = await run_in_threadpool(get_items_from_API, search)
+    products = await run_in_threadpool(get_items_test, search)
     return products
 
 

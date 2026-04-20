c8700f0fc01164abccb292a5a18b376840f39ec8
Olga Chirkova <lely4e@gmail.com>
2026-04-20 11:21:54 +0200

    Simplified text

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,9 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex items-center justify-center bottom-8 text-[12px] text-gray-500 mt-26">
       <p className="leading-relaxed">
-        ©2026 CHOOSR - Decision infrastructure for the indecisive.
-        <br />
-        Just votes.
+        ©2026 choosr. Decision infrastructure for the indecisive.
       </p>
     </footer>
   );
